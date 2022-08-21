a76e06d624cbf50ebac196f1d201516b489031a9
Andrew Barber <drewbarber72@gmail.com>
2022-08-21 06:08:25 -0400

    renamed folders to match exercise

---
+++ BestBuyBestPractices/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace BestBuyBestPractices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+            var repo = new DapperDepartmentRepository(conn);
+
+            //Console.WriteLine("Enter new department name");
+            //string newDept = Console.ReadLine();
+            //repo.InsertDepartment(newDept);
+
+            var depts = repo.GetAllDepartments();
+            foreach (var item in depts)
+            {
+                Console.WriteLine($"Department ID: {item.DepartmentID} Name: {item.Name}");
+            }
+
+        }
+    }
+}
